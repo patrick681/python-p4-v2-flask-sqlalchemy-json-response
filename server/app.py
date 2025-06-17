4eec0f401185c02febf14e9c1299725c50c0d59d
kamaupatrick332@gmail.com <youremail@example.com>
2025-06-18 01:49:46 +0300

    .

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 # server/app.py
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
 
 from models import db, Pet
@@ -9,6 +9,7 @@ from models import db, Pet
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
 
 migrate = Migrate(app, db)
 db.init_app(app)
@@ -16,17 +17,43 @@ db.init_app(app)
 
 @app.route('/')
 def index():
-    return make_response(
-        '<h1>Welcome to the pet directory!</h1>',
-        200
-    )
-
+    body = {'message': 'Welcome to the pet directory!'}
+    return jsonify(body), 200
 
 @app.route('/demo_json')
 def demo_json():
-    pet_json = '{"id": 1, "name" : "Fido", "species" : "Dog"}'
-    return make_response(pet_json, 200)
+    pet = Pet.query.first()
+    pet_dict = {'id': pet.id,
+                'name': 'Fido',
+                'species': 'Dog'
+                }
+    return jsonify(pet_dict), 200
+@app.route('/pets/<int:id>')
+def pet_by_id(id):
+    pet = Pet.query.filter(Pet.id == id).first()
 
+    if pet:
+        body = {'id': pet.id,
+                'name': pet.name,
+                'species': pet.species}
+        status = 200
+    else:
+        body = {'message': f'Pet {id} not found.'}
+        status = 404
+
+    return jsonify(body), status
+@app.route('/species/<string:species>')
+def pet_by_species(species):
+    pets = []  # array to store a dictionary for each pet
+    for pet in Pet.query.filter_by(species=species).all():
+        pet_dict = {'id': pet.id,
+                    'name': pet.name,
+                    }
+        pets.append(pet_dict)
+    body = {'count': len(pets),
+            'pets': pets
+            }
+    return jsonify(body), 200
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
